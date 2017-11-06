7ee68a52d80122ddc2d64b66a61f39e73bf727c7
ThomasAmann <thomasamann25@gmail.com>
2017-11-06 16:26:59 +0100

    Finished the first method

--- src/main/scala/models/impl/GamefieldGraph.scala
+++ src/main/scala/models/impl/GamefieldGraph.scala
@@ -5,10 +5,19 @@ import models.IGamefieldGraph
 class GamefieldGraph extends IGamefieldGraph {
 
   val NUMBER_VERTEX = 24
-  var vertexes = List[Char]
-  var adjacencyList = Seq[List[Int]]
+  val CHAR_VERTEXES = List('b', 'n', 's')
+  var vertexes = Array[Char](24)
+  var adjacencyList = List(List(Int))
 
-  override def setStoneVertex(vertex: Int, color: Char): Boolean = ???
+  override def setStoneVertex(vertex: Int, color: Char): Boolean = {
+    if (vertex >= 0 && vertex < 24 && CHAR_VERTEXES.contains(color)) {
+      vertexes(vertex) = color
+      return true
+    }
+    return false
+  }
+
+
 
   override def getStoneColorVertex(vertex: Int): Char = ???
 
