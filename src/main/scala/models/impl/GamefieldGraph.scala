5032bab64928c8a1ffa65d0d4f6afc73b009f65c
ThomasAmann <thomasamann25@gmail.com>
2017-11-06 16:53:33 +0100

    Add the second method and implement two const variables

--- src/main/scala/models/impl/GamefieldGraph.scala
+++ src/main/scala/models/impl/GamefieldGraph.scala
@@ -4,22 +4,33 @@ import models.IGamefieldGraph
 
 class GamefieldGraph extends IGamefieldGraph {
 
+  val NUMBER_ZERO = 0
   val NUMBER_VERTEX = 24
   val CHAR_VERTEXES = List('b', 'n', 's')
+  val CHAR_EMPTY = 'n'
   var vertexes = Array[Char](24)
   var adjacencyList = List(List(Int))
 
+  def createVertexes(): Unit = {
+
+    for(i <- NUMBER_ZERO to NUMBER_VERTEX)
+      vertexes(i) = CHAR_EMPTY
+  }
+
   override def setStoneVertex(vertex: Int, color: Char): Boolean = {
-    if (vertex >= 0 && vertex < 24 && CHAR_VERTEXES.contains(color)) {
+    if (vertex >= NUMBER_ZERO && vertex < NUMBER_VERTEX && CHAR_VERTEXES.contains(color)) {
       vertexes(vertex) = color
       return true
     }
     return false
   }
 
+  override def getStoneColorVertex(vertex: Int): Char = {
+    if (vertex >= NUMBER_ZERO && vertex < NUMBER_VERTEX)
+      return vertexes(vertex)
 
-
-  override def getStoneColorVertex(vertex: Int): Char = ???
+    return CHAR_EMPTY
+  }
 
   override def getAdjacencyList(v: Int): List[Int] = ??? //adjacencyList(v)
 
@@ -27,3 +38,4 @@ class GamefieldGraph extends IGamefieldGraph {
 
   override def setId(id: String): Unit = ???
 }
+
