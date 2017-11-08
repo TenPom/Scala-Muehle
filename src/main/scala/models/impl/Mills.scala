648991e5ad340b5301fb94c4b294dca64cde1b8f
Patrick Schmidt <schmidt.patrick.14@gmail.com>
2017-11-08 13:10:19 +0100

    scaladoc

--- src/main/scala/models/impl/Mills.scala
+++ src/main/scala/models/impl/Mills.scala
@@ -3,17 +3,42 @@ package models.impl
 import models.IMills
 import scala.io.Source
 
+/**
+  * Controls the mills on the gamefield
+  * @author Patrick Schmidt
+  * @since 06.11.2017
+  */
 class Mills extends IMills {
 
+  /**
+    * Number of Fields on the gamefield
+    */
   val NUMBER_VERTEX = 24
+
+  /**
+    * matrix of possible mills
+    */
   var millsArray : Array[MillsList] = new Array[MillsList](NUMBER_VERTEX)
 
   for (i <- 0 to millsArray.length) millsArray(i) = new MillsList()
 
+  /**
+    * Returns the first possible mill of the vertex
+    * @param v vertex
+    * @return the list of vertexes who are included in the mill
+    */
   override def getMill(v : Int) : List[Int] =  millsArray(v - 1).millList1
 
+  /**
+    * Returns the second possible mill of the vertex
+    * @param v vertex
+    * @return the list of vertexes who are included in the mill
+    */
   override def getMill2(v : Int) : List[Int] =  millsArray(v - 1).millList2
 
+  /**
+    * Maps the possible mills to the list
+    */
   def fillMillsArray : Unit = {
     for (line <- Source.fromFile("/Util/mills.txt").getLines()) {
       val node = line.split(" ")(0).toInt
@@ -28,6 +53,9 @@ class Mills extends IMills {
 
 }
 
+/**
+  * Helperclass for possible mills
+  */
 class MillsList {
   var millList1 : List[Int] = List()
   var millList2 : List[Int] = List()
