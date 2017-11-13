e2c10ba9eb0ee66c725bfed3f40138c9712823d3
Patrick Schmidt <schmidt.patrick.14@gmail.com>
2017-11-13 11:38:00 +0100

    changed return type

--- src/main/scala/models/impl/Player.scala
+++ src/main/scala/models/impl/Player.scala
@@ -6,7 +6,7 @@ class Player (nameP:String, colorP:Char) extends IPlayer {
   var name : String = nameP
   var color : Char = colorP
 
-  override def getName = name
+  override def getName : String = this.name
 
-  override def getColor = color
+  override def getColor : Char = this.color
 }
